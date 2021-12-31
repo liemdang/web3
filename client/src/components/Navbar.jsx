9b1810361948cef2bd595671c12db5ca5f7e6669
liem <liem.dang@hotmail.com>
2021-12-31 17:10:52 +0100

    fixed typo

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
                 }
                 { toggleMenu && (
                     <ul className="z-10 fixed top-0 -right-2 p-3 w-[70vw] h-screen shadow-2xl md:hidden list-none
-                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slid-in">
+                    flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in">
                         <li className="text-xl w-full my-2">
                             <AiOutlineClose onClick={() => setToggleMenu(false)} />
                         </li>
